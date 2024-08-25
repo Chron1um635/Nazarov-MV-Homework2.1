1b7372237f52a0a2957e06fc4d062280492529b5
Chron1um635 <chron1um522@gmail.com>
2024-08-25 16:11:19 +0300

    Initial Commit

---
+++ Nazarov MV Homework2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nazarov MV Homework2.1
+//
+//  Created by Максим Назаров on 25.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
